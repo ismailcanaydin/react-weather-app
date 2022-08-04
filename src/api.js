04ae97e97d7ac17045f45a1fc79ab5158ab3671a
ismailcanaydin <ismailcanaydin@gmail.com>
2022-08-04 17:45:01 +0300

    search

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '164232cd25mshc977a9de3ea641ap11aa74jsnff477cc144d1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
